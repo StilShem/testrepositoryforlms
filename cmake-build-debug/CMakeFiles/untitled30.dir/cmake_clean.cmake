571940eb34cb3187af888f603763f8b90f8b8f95
StilShem <avkrug96@gmail.com>
2023-04-18 22:27:30 +0500

    Like a first commit

---
+++ cmake-build-debug/CMakeFiles/untitled30.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled30.dir/main.cpp.obj"
+  "CMakeFiles/untitled30.dir/main.cpp.obj.d"
+  "libuntitled30.dll.a"
+  "untitled30.exe"
+  "untitled30.exe.manifest"
+  "untitled30.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
